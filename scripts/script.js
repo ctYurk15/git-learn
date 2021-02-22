feb1f43d4c692a8c157d7922854b75efd94e5b61
ctYurk15 <levgenetic@gmail.com>
2021-02-22 19:22:03 +0200

    Added two investment calculators

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ function sideDivDisp()
     else document.getElementById("p2").style.display = "none";
 }
 
-function cSE()
+function cSE() //calculate equation like x^2+2x+4=0
 {
     //getting values
     var a = document.getElementById("ise1").value;
@@ -40,7 +40,7 @@ function cSE()
     document.getElementById('resultPSE').innerHTML = result;
 }
 
-function cPT()
+function cPT() //get one side of triangle with 90 angle
 {
     //getting values
     var a = document.getElementById("ipt1").value;
@@ -59,14 +59,14 @@ function cPT()
     document.getElementById('resultPPT').innerHTML = result;
 }
 
-function cEDT()
+function cEDT() //kill time CSGO
 {
     var option = document.getElementById('weaponsOptionCS').value; //getting chosed weapons value(damage per second)
     
     document.getElementById('resultPEDT').innerHTML = "За "+(100/option).toFixed(2)+"c";
 }
 
-function cPIW()
+function cPIW() //loot weight pugb
 {
     var options = document.getElementsByName('pubgItem'); //getting all the options
     var result = 0;
@@ -81,4 +81,34 @@ function cPIW()
     
     //printing result
     document.getElementById('resultPPIW').innerHTML = "Загалом: "+result+"кг"
+}
+
+function cCP() //bank deposit profit
+{
+    //getting all data user logged
+    var time = parseInt(document.getElementById('depositTime').value);
+    var money = parseInt(document.getElementById('depositInvestment').value);
+    var yearProcent = parseFloat(document.getElementById('depositProcent').value);
+    
+    //getting one month procent
+    var monthProcent = (Math.pow(((100+yearProcent)/100), (1/12))-1)*100;
+    
+    //calculating total deposit
+    var total = (money*Math.pow((1+(monthProcent/100)), time)).toFixed(2);
+    document.getElementById('resultPCP').innerHTML = "За "+time+" місяців на депозиті буде "+total;
+}
+
+function cCSP() //company stocks profit !!!!!FIX IT!!!!!!!
+{
+    //getting all data user logged
+    var time = parseInt(document.getElementById('stockTime').value);
+    var money = parseInt(document.getElementById('stockInvestment').value);
+    var procent = parseFloat(document.getElementById('stockProcent').value);
+    
+    //calculating year statistic
+    var yearTotal = 100*Math.pow((1+(procent/100)), 4);
+    var yearProcent = yearTotal-100;
+    
+    var total = money*Math.pow((1+(yearProcent/100)), time);
+    document.getElementById('resultPCSP').innerHTML = "За "+time+" років у вас буде "+total;
 }
