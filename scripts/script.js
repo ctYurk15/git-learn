14d7f256f47ece0ad6bfd6e077ac1e1967b06e3b
ctYurk15 <levgenetic@gmail.com>
2021-02-27 13:11:33 +0200

    Changed functions and some id names

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ function sideDivDisp()
     else document.getElementById("p2").style.display = "none";
 }
 
-function cSE() //calculate equation like x^2+2x+4=0
+function calculateSquareEquation() //calculate equation like x^2+2x+4=0
 {
     //getting values
     var a = document.getElementById("ise1").value;
@@ -37,10 +37,10 @@ function cSE() //calculate equation like x^2+2x+4=0
     }
     
     //showing result
-    document.getElementById('resultPSE').innerHTML = result;
+    document.getElementById('resultPSquareEquation').innerHTML = result;
 }
 
-function cPT() //get one side of triangle with 90 angle
+function calculateTriangleSide() //get one side of triangle with 90 angle
 {
     //getting values
     var a = document.getElementById("ipt1").value;
@@ -53,13 +53,13 @@ function cPT() //get one side of triangle with 90 angle
     b = parseFloat(b);
     
     var c = Math.pow((Math.pow(a, 2) + Math.pow(b, 2)), 0.5);
-    result += "Гіпотенуза = "+c;
+    result += "Гіпотенуза = "+c.toFixed(2);
     
     //showing result
-    document.getElementById('resultPPT').innerHTML = result;
+    document.getElementById('resultPTriangleSide').innerHTML = result;
 }
 
-function cNC() //how much numbers we can make 
+function calculateCombinationsCount() //how much numbers we can make 
 {
     //getting value
     var numbersCount = document.getElementById('inc').value;
@@ -73,17 +73,17 @@ function cNC() //how much numbers we can make
         else result *= (numbersCount-i);
     }
     
-    document.getElementById('resultPCNC').innerHTML = "Загалом можна скласти " + result + " комбінацій";
+    document.getElementById('resultPCombinationsCount').innerHTML = "Загалом можна скласти " + result + " комбінацій";
 }
 
-function cEDT() //kill time CSGO
+function calculateEnemyDeathTime() //kill time CSGO
 {
     var option = document.getElementById('weaponsOptionCS').value; //getting chosed weapons value(damage per second)
     
-    document.getElementById('resultPEDT').innerHTML = "За "+(100/option).toFixed(2)+"c";
+    document.getElementById('resultPEnemyDeathTime').innerHTML = "За "+(100/option).toFixed(2)+"c";
 }
 
-function cPIW() //loot weight pugb
+function calculatePUBGLootWeight() //loot weight pugb
 {
     var options = document.getElementsByName('pubgItem'); //getting all the options
     var result = 0;
@@ -97,10 +97,10 @@ function cPIW() //loot weight pugb
     }
     
     //printing result
-    document.getElementById('resultPPIW').innerHTML = "Загалом: "+result+"кг"
+    document.getElementById('resultPPUBGItemsWeight').innerHTML = "Загалом: "+result+"кг"
 }
 
-function cSC() //smelting furnace calculator
+function calculateFuelCount() //smelting furnace calculator
 {
     var fuelEffectivity = document.getElementById('fuelOption').value;
     var items = document.getElementById('objectCount').value;
@@ -110,14 +110,14 @@ function cSC() //smelting furnace calculator
     var value = e.options[e.selectedIndex].value;
     var text = e.options[e.selectedIndex].text;
     
-    document.getElementById('resultPSC').innerHTML = "Необхідно витратити " + parseInt(items/fuelEffectivity) + " " + text;
+    document.getElementById('resultPFuelCount').innerHTML = "Необхідно витратити " + parseInt(items/fuelEffectivity) + " " + text;
 }
 
-function cCP() //bank deposit profit
+function calculateDepositIncome() //bank deposit profit
 {
     //getting all data user logged
     var time = parseInt(document.getElementById('depositTime').value);
-    var money = parseInt(document.getElementById('depositInvestment').value);
+    var money = parseFloat(document.getElementById('depositInvestment').value);
     var yearProcent = parseFloat(document.getElementById('depositProcent').value);
     
     //getting one month procent
@@ -125,28 +125,28 @@ function cCP() //bank deposit profit
     
     //calculating total deposit
     var total = (money*Math.pow((1+(monthProcent/100)), time)).toFixed(2);
-    document.getElementById('resultPCP').innerHTML = "За "+time+" місяців на депозиті буде "+total;
+    document.getElementById('resultPDepositeIncome').innerHTML = "За "+time+" місяців на депозиті буде "+total;
 }
 
-function cCSP() //company stocks profit !!!!!FIX IT!!!!!!!
+function calculateStonksIncome() //company stocks profit !!!!!FIX IT!!!!!!!
 {
     //getting all data user logged
     var time = parseInt(document.getElementById('stockTime').value);
-    var money = parseInt(document.getElementById('stockInvestment').value);
+    var money = parseFloat(document.getElementById('stockInvestment').value);
     var procent = parseFloat(document.getElementById('stockProcent').value);
     
     //calculating year statistic
     var yearTotal = 100*Math.pow((1+(procent/100)), 4);
     var yearProcent = yearTotal-100;
     
-    var total = money*Math.pow((1+(yearProcent/100)), time);
-    document.getElementById('resultPCSP').innerHTML = "За "+time+" років у вас буде "+total;
+    var total = (money*Math.pow((1+(yearProcent/100)), time)).toFixed(2);
+    document.getElementById('resultPStoksIncome').innerHTML = "За "+time+" років у вас буде "+total;
 }
 
-function cS() //savings calculator
+function calculateSavings() //savings calculator
 {
-    var money = parseInt(document.getElementById('savingsCount').value);
+    var money = parseFloat(document.getElementById('savingsCount').value);
     var time = parseInt(document.getElementById('savingsTime').value);
     
-    document.getElementById('resultPS').innerHTML = "За "+time+" місяців у вас буде "+time*money;
+    document.getElementById('resultPSavings').innerHTML = "За "+time+" місяців у вас буде "+(time*money).toFixed(2);
 }
